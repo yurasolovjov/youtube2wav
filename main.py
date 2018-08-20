b4d664cd8080e7865faad72750462ec38e09c0f1
yurasolovjov <yura.solovjov@mail.ru>
2018-08-20 14:55:18 +0300

    fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 import youtube_dl
+import json
 
 
 ydl_opts = {
@@ -10,5 +11,33 @@ ydl_opts = {
         'preferredquality': '192',
     }],
 }
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+
+def getListFiles(json_file, filter):
+
+    listFiles = list()
+    names = list()
+
+    with open(json_file) as f:
+        config = json.load(f)
+
+        for data in config:
+            name = data["name"]
+
+            if name == filter:
+                return  data["positive_examples"]
+
+def main():
+
+    ls = getListFiles('D:\\repo\\ontology\\ontology.json',"Glass")
+
+    for data in ls:
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                # ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+                ydl.download([data])
+        except:
+            print("Can`t load file")
+            continue
+
+if __name__ == "__main__":
+    main()
